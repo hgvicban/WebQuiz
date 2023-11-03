483630e4b41a7c50b8c1bd66a47de5c07a8e146a
Grace Icban <hgvicban@gmail.com>
2023-11-02 21:14:58 -0700

    update quizzes endpoint

--- src/main/java/com/hgvicban/simplequiz/controller/QuizController.java
+++ src/main/java/com/hgvicban/simplequiz/controller/QuizController.java
@@ -1,21 +1,46 @@
 package com.hgvicban.simplequiz.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hgvicban.simplequiz.model.Quiz;
 import com.hgvicban.simplequiz.model.Response;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class QuizController {
-    ObjectMapper objectMapper = new ObjectMapper();
 
-    @PostMapping("/api/quiz")
-    public String postAnswer(@RequestParam int answer) throws JsonProcessingException {
-        boolean success = answer == 2;
+    Map<Integer, Quiz> quizzes = new HashMap<>();
 
-        Response response = new Response(success);
-        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response);
+    @PostMapping("/quizzes")
+    public Quiz postQuiz(@RequestBody Quiz quiz) throws JsonProcessingException {
+        quiz.setId(quizzes.size());
+        quizzes.put(quiz.getId(), quiz);
+        return quiz;
+    }
+
+    @PostMapping("/quizzes/{id}/solve")
+    public Response postAnswer(@PathVariable int id, @RequestParam int answer) throws JsonProcessingException {
+        if (quizzes.containsKey(id)) return new Response(quizzes.get(id).checkAnswer(answer));
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/quizzes/{id}")
+    public Quiz getQuizById(@PathVariable int id) throws JsonProcessingException {
+        if (quizzes.containsKey(id)) return quizzes.get(id);
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/quizzes")
+    public List<Quiz> getQuizzes() throws JsonProcessingException {
+        return new ArrayList<>(quizzes.values());
     }
 }
